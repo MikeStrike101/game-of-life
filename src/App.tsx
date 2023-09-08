4807394c6c05c69780013d058222687cec1255ef
MikeStrike101 <mihaialexandruagache@yahoo.com>
2023-09-09 01:28:46 +0300

    added a countNeighbors function

--- src/App.tsx
+++ src/App.tsx
@@ -24,6 +24,15 @@ const generateEmptyGrid = () => {
       return rows;
 };
 
+const countNeighbors = (grid: any[][], x: number, y: number) => {
+  return operations.reduce((acc, [i, j]) => {
+    const row = (x + i + numRows) % numRows;
+    const col = (y + j + numCols) % numCols;
+    acc += grid[row][col];
+    return acc;
+  }, 0);
+};
+
 const App: React.FC = () => {
   const [grid, setGrid] = useState(() => {
      return generateEmptyGrid(); 
@@ -44,21 +53,14 @@ const runSimulation = useCallback(() => {
         return produce(g, gridCopy => {
       for (let i = 0; i < numRows; i++) {
           for (let j = 0; j < numCols; j++) {
-             let neighbors = 0;
-             operations.forEach(([x, y]) => {
-               const newI = i + x;
-               const newJ = j + y;
-               if (newI >= 0 && newI < numRows && newJ >= 0 && newJ < numCols) {
-                  neighbors += g[newI][newJ];
-               }
-             });
+            const neighbors = countNeighbors(g, i, j);
              if (neighbors < 2 || neighbors > 3) {
                 gridCopy[i][j] = 0;
              } else if (g[i][j] === 0 && neighbors === 3) {
                  gridCopy[i][j] = 1;
              }
             }
-      }
+          }
         });
       });
       setTimeout(runSimulation, 1000);
